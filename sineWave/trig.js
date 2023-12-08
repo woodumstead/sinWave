a1f79585c6cb0b13740dc5c5b16c62ae608abbdd
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2023-12-08 12:29:20 -0700

    Add files via upload

---
+++ sineWave/trig.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    let canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d'),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight;
+
+    context.translate(0, height / 2);
+    context.scale(1, -1);
+
+
+    for (let angle = 0; angle < Math.PI * 2; angle += .01) {
+        console.log(Math.sin(angle));
+        let x = angle * 200,
+            y = Math.sin(angle) * 200;
+
+        context.fillRect(x, y, 5, 5);
+    }
+
+};
+
